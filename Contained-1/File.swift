47d11c2c0d33eb966612fa3b6048815722aac4d2
Taylor Lyles <hawks93187@gmail.com>
2019-04-29 12:17:36 -0700

    First commit

---
+++ Contained-1/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Contained-1
+//
+//  Created by Taylor Lyles on 4/29/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+
+import Foundation
+
+class Settings {
+    var shouldRoll = false
+    var shouldZoom = false
+}
