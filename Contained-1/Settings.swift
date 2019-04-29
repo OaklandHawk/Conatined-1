38485ac35a0e2e3a686b73cbf6d415469cdd9100
Taylor Lyles <hawks93187@gmail.com>
2019-04-29 12:50:27 -0700

    Building the In fo Sequence

--- Contained-1/Settings.swift
+++ Contained-1/Settings.swift
@@ -6,10 +6,10 @@
 //  Copyright © 2019 Taylor Lyles. All rights reserved.
 //
 
-
-import Foundation
-
 class Settings {
+    static let shared = Settings()
+    private init() {}
+    
     var shouldRoll = false
     var shouldZoom = false
 }
